e875c8ed255c02b76a4cf7b759e5852a8595054a
pries002 <pries002@CZ220039.cz.kostal.int>
2024-07-23 10:53:07 +0200

    update

--- Scarecrow/src/app.c
+++ Scarecrow/src/app.c
@@ -12,69 +12,94 @@
 #include "timer.h"
 #include "watchdog.h"
 
-#define BEEP_PAUSE_MS       5000
+#define BEEP_PAUSE_MS       15000
+
+typedef enum
+{
+  APP_MODE_BEEP,
+  APP_MODE_ALARM,
+} app_mode_e;
+
+static app_mode_e eMode;
 
 i2s_drv_t* pI2s = i2s2;
 spi_drv_t* pSpi = spi1;
 
 void _PlaySound(uint8_t nFileNumber);
 void _PlaySound16bit(const uint16_t* const pData, const uint32_t nSamples);
+void _BeepMode(void);
+void _AlarmMode(void);
 
 void App_Init(void)
 {
   HW_Init();
-
+  eMode = APP_MODE_BEEP;
 }
 
 void App_Exec(void)
 {
-  // beep files 1-7
-  // alarm files 10-18
 
+  switch (eMode)
+  {
+  case APP_MODE_BEEP:
+    _BeepMode();
+    break;
+  case APP_MODE_ALARM:
+    _AlarmMode();
+    eMode = APP_MODE_BEEP;
+    Timer_Delay_ms(1000);
+    break;
+  default:
+    break;
+  }
+
+  // wait time between beep sounds
+  uint32_t nEndBeepTime = Timer_GetTicks_ms() + BEEP_PAUSE_MS;
+  while (Timer_GetTicks_ms() < nEndBeepTime)
+  {
+    __WFI();
+    if (HW_IsPirActive() == true)
+    {
+      nEndBeepTime = Timer_GetTicks_ms();
+      eMode = APP_MODE_ALARM;
+    }
+  }
+
+}
+
+void _BeepMode(void)
+{
+  // beep files 1-7
   const uint8_t arrBeepSounds[] =
   {
       1, 2, 3, 4, 5, 6, 7, 4, 2, 1, 7, 3, 6, 5
   };
 
+  static uint32_t nBeepIdx = 0;
+
+  _PlaySound(arrBeepSounds[nBeepIdx]);
+  nBeepIdx++;
+  if (nBeepIdx > sizeof(nBeepIdx))
+  {
+    nBeepIdx = 0;
+  }
+}
+
+void _AlarmMode(void)
+{
+  // alarm files 10-18
   const uint8_t arrAlarmSounds[] =
   {
       10, 11, 12, 13, 14, 15, 16, 17, 18,
   };
+  static uint8_t nAlarmIdx = 0;
 
-  uint8_t nAlarmIdx = 0;
-
-  for (uint8_t i = 0; i < sizeof (arrBeepSounds); i++)
+  _PlaySound(nAlarmIdx);
+  nAlarmIdx++;
+  if (nAlarmIdx > sizeof(arrAlarmSounds))
   {
-    _PlaySound(arrBeepSounds[i]);
-
-    // wait time between beep sounds
-    uint32_t nEndBeepTime = Timer_GetTicks_ms() + BEEP_PAUSE_MS;
-    while (Timer_GetTicks_ms() < nEndBeepTime)
-    {
-
-      __WFI();
-
-      WDG_SimpleRefresh();
-
-      if (HW_IsPirActive() == true)
-      {
-        nEndBeepTime = Timer_GetTicks_ms();
-      }
-    }
-
-    // loop for alarm sound
-    while (HW_IsPirActive() == true)
-    {
-      _PlaySound(nAlarmIdx);
-      Timer_Delay_ms(500);
-      nAlarmIdx++;
-      if (nAlarmIdx > sizeof(arrAlarmSounds))
-      {
-        nAlarmIdx = 0;
-      }
-    }
+    nAlarmIdx = 0;
   }
-
 }
 
 void _PlaySound(uint8_t nFileNumber)
